3802dbfde9f9e48c247335361af355b74c7d615e
sadKira <kiraygm.12@gmail.com>
2026-01-11 17:28:40 +0800

    Fixed flickering; logger db progress

--- resources/views/livewire/logger/logger-dashboard.blade.php
+++ resources/views/livewire/logger/logger-dashboard.blade.php
@@ -1,18 +1,18 @@
-<div>
-    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-            <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-            </div>
-        </div>
-        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-            <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-        </div>
+<div class="max-w-4xl mx-auto px-4">
+     {{-- App Header --}}
+    <div class="flex items-center justify-between mb-10">
+
+        {{-- Breadcrumbs --}}
+        <x-ui.breadcrumbs>
+            <x-ui.breadcrumbs.item>Kiroku</x-ui.breadcrumbs.item>
+            <x-ui.breadcrumbs.item href="{{ route('logger_dashboard') }}">Dashboard</x-ui.breadcrumbs.item>
+        </x-ui.breadcrumbs>
+
     </div>
+
+    <div class="mt-5">
+        <flux:heading size="lg">Student Logs</flux:heading>
+    </div>
+
+    
 </div>
