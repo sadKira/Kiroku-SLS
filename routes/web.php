57d484f77af7cf87fd165fa64f8017f22d7b91e4
sadKira <kiraygm.12@gmail.com>
2026-01-15 00:36:13 +0800

    99% account management; in progress

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Livewire\Management\AdminDashboard;
 use App\Livewire\Management\StudentList;
 use App\Livewire\Management\AboutKiroku;
 use App\Livewire\Management\StudentLogs;
+use App\Livewire\Management\UserAccounts;
 
 use App\Livewire\Logger\LoggerDashboard;
 use App\Livewire\Logger\ViewLogs;
@@ -22,8 +23,10 @@ Route::middleware(['auth', 'admin'])->group(function () {
 
     Route::get('/admin-dashboard', AdminDashboard::class)->name('admin_dashboard');
     Route::get('/student-list', StudentList::class)->name('student_list');
-    Route::get('/student-logs', StudentLogs::class)->name('student_logs');
     Route::get('/about-kiroku', AboutKiroku::class)->name('about_kiroku');
+
+    Route::get('/student-logs', StudentLogs::class)->name('student_logs');
+    Route::get('/user-accounts', UserAccounts::class)->name('user_accounts');
 
     Route::get('/export-barcode', [ExportBarcode::class, 'generatePdf'])->name('export_barcode');
     Route::get('/export-student-logs', [ExportStudentLogs::class, 'generatePdf'])->name('export_student_logs');
