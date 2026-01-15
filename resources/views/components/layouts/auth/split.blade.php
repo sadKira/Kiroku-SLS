eac7dab571e1737d9ed0ba4f13b4a4fd099f4bb1
sadKira <kiraygm.12@gmail.com>
2026-01-15 15:12:54 +0800

    Modified auth layout

--- resources/views/components/layouts/auth/split.blade.php
+++ resources/views/components/layouts/auth/split.blade.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="">
     <head>
         @include('partials.head')
     </head>
+    <script>
+        // Ensure the 'dark' class is removed from the html element
+        document.documentElement.classList.remove('dark');
+    </script>  
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
 
